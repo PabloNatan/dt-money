48cf2c4df536a7f72f4b92fbfd8e55f4a90b0358
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-02-13 07:13:03 -0300

    feat: add axios to project

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import * as zod from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as S from "./styles";
+import { useTransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = zod.object({
   query: zod.string(),
@@ -11,6 +12,8 @@ const searchFormSchema = zod.object({
 type SearchFormInputs = zod.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useTransactionsContext();
+
   const {
     register,
     handleSubmit,
@@ -19,7 +22,9 @@ export function SearchForm() {
     resolver: zodResolver(searchFormSchema),
   });
 
-  function handleSearchTransactions(data: SearchFormInputs) {}
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query);
+  }
 
   return (
     <S.SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
